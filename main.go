374a2b06f4d7c1ab68f9a8b26d97fb7384d8cd31
Wicken <ericghoubiguian@live.fr>
2022-04-18 17:17:13 +0200

    For future comments...

--- main.go
+++ main.go
@@ -1,18 +1,28 @@
 package main
 
+//
 import "github.com/gin-gonic/gin"
 
+//
 func main() {
+	
+	//
 	r := gin.Default()
+	
+	//
 	r.GET("", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "Hello, world!",
 		})
-	}) 
+	})
+	
+	//
 	r.GET("/test", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "ping pong",
 		})
 	})
+	
+	//
 	r.Run() // listen and serve on 0.0.0.0:8080...
 }
