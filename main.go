94a77d48696a902ff7e92ad5f467cc61829a6752
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2022-04-30 07:43:20 +0200

    HJH

--- main.go
+++ main.go
@@ -5,24 +5,38 @@ import "github.com/gin-gonic/gin"
 
 //
 func main() {
-	
+
 	//
 	r := gin.Default()
-	
+
 	//
 	r.GET("", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "Hello, world!",
 		})
 	})
-	
+
 	//
-	r.GET("/test", func(c *gin.Context) {
+	r.GET("/all", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "ping pong",
 		})
 	})
-	
+
+	//
+	r.GET("/all", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "ping pong",
+		})
+	})
+
+	//
+	r.GET("/search", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "ping pong",
+		})
+	})
+
 	//
 	r.Run() // listen and serve on 0.0.0.0:8080...
 }
