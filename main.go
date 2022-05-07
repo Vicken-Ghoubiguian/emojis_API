820527b158156b56aeaca168a41a89401bf75250
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2022-05-07 11:21:05 +0200

    Erix

--- main.go
+++ main.go
@@ -37,5 +37,12 @@ func main() {
 	})
 
 	//
+	r.GET("/all_sub_categories", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "ping pong",
+		})
+	})
+
+	//
 	r.Run() // listen and serve on 0.0.0.0:8080...
 }
