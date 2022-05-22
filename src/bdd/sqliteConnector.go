fb4076de5da4a21e687d7d05d1543b6a97afe6ab
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2022-05-22 09:20:42 +0200

    JuiOOPP

--- src/bdd/sqliteConnector.go
+++ src/bdd/sqliteConnector.go
@@ -1,11 +1,33 @@
 package bdd
 
 //
-//import (
-//"database/sql"
-//"github.com/mattn/go-sqlite3"
-//)
+import (
+	"database/sql"
+)
 
 //
 type sqliteConnector struct {
+}
+
+//
+func (currentSqliteConnector *sqliteConnector) InitializeSqlConnector() {
+
+}
+
+//
+func (currentSqliteConnector *sqliteConnector) getEmoji() {
+
+	db, err := sql.Open("sqlite3", "./foo.db")
+
+	checkErr(err)
+
+	db.Close()
+}
+
+func checkErr(err error) {
+
+	if err != nil {
+
+		panic(err)
+	}
 }
