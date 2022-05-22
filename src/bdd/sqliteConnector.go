a082bc07eebf524ece8c743c51ca0ee3bbc28c0c
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2022-05-22 09:39:51 +0200

    yaldy

--- src/bdd/sqliteConnector.go
+++ src/bdd/sqliteConnector.go
@@ -6,11 +6,19 @@ import (
 )
 
 //
-func getEmoji() {
+func getEmoji(emoji_id int64) {
 
 	db, err := sql.Open("sqlite3", "./unicode_emojis.db")
 
 	checkErr(err)
+
+	rows, err := db.Query("SELECT * FROM unicode_emojis WHERE  = ?")
+
+	checkErr(err)
+
+	for rows.Next() {
+
+	}
 
 	db.Close()
 }
