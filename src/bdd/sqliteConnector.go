0fe63944b0b2d55348c5ed3eee6d3ef75b7d0ee2
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2022-05-22 09:48:07 +0200

    Onk

--- src/bdd/sqliteConnector.go
+++ src/bdd/sqliteConnector.go
@@ -9,17 +9,14 @@ import (
 func getEmoji(emoji_id int64) {
 
 	db, err := sql.Open("sqlite3", "./unicode_emojis.db")
-
 	checkErr(err)
 
 	stmt, err := db.Prepare("SELECT * FROM unicode_emojis WHERE  = ?")
 
 	rows, err := stmt.Exec("astaxieupdate", emoji_id)
-
 	checkErr(err)
 
 	print(rows)
-
 	checkErr(err)
 
 	db.Close()
