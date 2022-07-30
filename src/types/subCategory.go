562e59cbae53ce81f8f43a235e412a68fb291277
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2022-07-30 09:11:38 +0200

    HNJ

--- src/types/subCategory.go
+++ src/types/subCategory.go
@@ -2,4 +2,8 @@ package types
 
 // Defining the type 'SubCategory' which define an emojis' subcategory...
 type SubCategory struct {
+	number int
+	name string
+	description string
+	category Category
 }
