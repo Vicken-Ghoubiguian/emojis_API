4e182c2b1959247894ccf77b3507ebfdfe8ded42
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2022-07-30 19:57:23 +0200

    JKJ

--- src/types/emoji.go
+++ src/types/emoji.go
@@ -15,3 +15,33 @@ func (current_emoji *Emoji) GetNumber() int {
 
 	return current_emoji.number
 }
+
+//
+func (current_emoji *Emoji) GetTitle() string {
+
+	return current_emoji.title
+}
+
+//
+func (current_emoji *Emoji) GetUnicode() string {
+
+	return current_emoji.unicode
+}
+
+//
+func (current_emoji *Emoji) GetCldr_short_name() string {
+
+	return current_emoji.cldr_short_name
+}
+
+//
+func (current_emoji *Emoji) GetCategory() Category {
+
+	return current_emoji.category
+}
+
+//
+func (current_emoji *Emoji) GetSubcategory() SubCategory {
+
+	return current_emoji.subcategory
+}
